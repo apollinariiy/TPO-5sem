f0372d3debf2eb60f29078249fd49f01ccb550f3
apollinariiy <95570468+apollinariiy@users.noreply.github.com>
2023-10-20 22:51:37 +0300

    commit java

---
+++ demolab3/src/main/java/com/example/demolab3/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.demolab3;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
