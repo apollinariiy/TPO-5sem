694f65aff3bf3caacbfcc0622b1f443f01eb9291
apollinariiy <95570468+apollinariiy@users.noreply.github.com>
2023-11-11 10:33:25 +0300

    fix

--- lr9/SeleniumTests/UnitTest1.cs
+++ lr9/SeleniumTests/UnitTest1.cs
@@ -21,7 +21,7 @@ namespace SeleniumTests
         {
             options = new ChromeOptions();
             driver = new ChromeDriver("C:\\webdriver\\chromedriver-win64", options);
-            baseURL = "https://megatop.by/catalog/zhenshchiny/galantereya";
+            baseURL = "https://megatop.by/catalog/zhenshchiny/galantereya/sumki";
             verificationErrors = new StringBuilder();
            
         }
@@ -45,15 +45,29 @@ namespace SeleniumTests
         public void Test1()
         {
             driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div/div/div/div/div/div/div[2]/button[1]")).Click();
 
-            Assert.That(driver.Title, Is.EqualTo("Женская галантерея — купить в интернет-магазине Мегатоп"));
-            driver.FindElement(By.ClassName("basket")).Click();
+            Thread.Sleep(1000);
+            IWebElement element = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div/div/div/div/div/div/div[2]/button[1]"));
+            if (element != null)
+            {
+                element.Click();
+            }
 
-            Assert.That(driver.Title, Is.EqualTo("Женская галантерея — купить в интернет-магазине Мегатоп"));
+            Assert.That(driver.Title, Is.EqualTo("Купить сумки женские в Минске, цена"));
+            driver.FindElement(By.CssSelector("button.basket")).Click();
+
+            Assert.That(driver.Title, Is.EqualTo("Купить сумки женские в Минске, цена"));
 
             driver.Navigate().GoToUrl("https://megatop.by/cart");
+
+
+/*            IWebElement element1 = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div/div/div/div/div/div/div[2]/button[1]"));
+            if (element1 != null)
+            {
+                element1.Click();
+            }*/
             Assert.That(driver.Title, Is.EqualTo("Оформление заказа MEGATOP"));
+            Thread.Sleep(1000);
             Assert.That(driver.FindElement(By.CssSelector(".mr-3.my-auto")).Text, Is.EqualTo("1 шт."));
         }
     }
